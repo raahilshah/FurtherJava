cad659f1869597ce729bf3655e90e924c8f346e1
Raahil Shah <rds46@cam.ac.uk>
2014-10-28 18:35:46 +0000

    Hello exercise completed.

--- src/uk/ac/cam/rds46/fjava/tick1/HelloWorld.java
+++ src/uk/ac/cam/rds46/fjava/tick1/HelloWorld.java
@@ -1,5 +1,10 @@
 package uk.ac.cam.rds46.fjava.tick1;
 
 public class HelloWorld {
-
+	public static void main(String[] args) {
+		if (args.length  < 1)
+			System.out.println("Hello, world");
+		else 
+			System.out.println("Hello, " + args[0]);
+	}
 }
